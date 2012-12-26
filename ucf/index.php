0f4300e88a8f12550f3f30c5977e04a312852147
Tommy Lee <tommy.t.lee@gmail.com>
2012-12-25 21:30:34 -0500

    initial commit

---
+++ ucf/index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8" />
+
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+
+  <title>Society of Physics Students - UCF </title>
+
+  <!-- Included CSS Files (Uncompressed) -->
+  <!--
+  <link rel="stylesheet" href="stylesheets/foundation.css">
+-->
+
+<!-- Included CSS Files (Compressed) -->
+<link rel="stylesheet" href="../stylesheets/foundation.min.css">
+<link rel="stylesheet" href="../stylesheets/foundation.custom.css">
+<link rel="stylesheet" href="../stylesheets/app.css">
+
+<script src="../javascripts/modernizr.foundation.js"></script>
+</head>
+<body>
+
+  <!-- Header and Nav -->
+  <?php include 'includes/header.php'; ?>
+  
+
+  <!-- End Header and Nav -->
+
+  <!-- Main Grid Section -->
+  <div class="row" style="background-color:#6E747A">
+    <div class="one columns">
+    </div>
+    <div class="ten columns">
+      <div id="slider">
+        <img src="../images/sps/frontpage/group1.jpg" />
+        <img src="../images/sps/frontpage/group2.jpg" />
+        <img src="../images/sps/frontpage/group3.jpg" />
+      </div>
+      <hr />
+    </div>
+    <div class="one columns">
+    </div>
+  </div>
+
+  <div class="row">
+
+    <div class="six columns">
+      <div class="panel">
+        <dl class="tabs">
+          <dd class="active"><a href="#simple1">Membership</a></dd>
+          <dd><a href="#simple2">Meetings</a></dd>
+          <dd><a href="#simple3">Mission</a></dd>
+        </dl>
+
+        <ul class="tabs-content">
+          <li class="active" id="simple1Tab">
+            <h5>Welcome </h5>
+            <p>Please visit the <a href="http://www.spsnational.org/about/benefits.htm">SPS Membership</a> page for information on how to join the Society of Physics students. Benefits include subscription to <b>Physics Today</b> magazine and one of its online journals. You must be a member if you with to participate in this local chapter. </p>
+          </li>
+          <li id="simple2Tab">
+            <h5>SPS Meetings</h5>
+            <p>We have meetings every other Tuesday in the Physical Sciences building (PSB) room 456. To be added to the mailing list for meetings and SPS group events please send an email to sps(at)ucf(dot)gmail(dot)com.
+              Please stay tuned for the Fall 2012 meeting schedule. </p>
+            </li>
+            <li id="simple3Tab">
+              <h5>Mission</h5>
+              The purpose of this Chapter is the advancement and diffusion of knowledge of the science of physics and serious encouragement of student interest in physics throughout the academic and local communities.</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="six columns">
+         <h4> Announcements </h4>
+         <hr />
+         <p> Dr. Costas Efthimiou accepts applications for ΣΠΣ, the Physics Honor Socienty. Those who have successfully completed several upper level physics courses, have a 3.0 overall GPA, and a 3.5 GPA in math and physics may bring an official transcipt to PSB 131 for Dr. Efthimiou to review. </p>
+         <hr />
+
+         <iframe src="https://www.google.com/calendar/embed?src=tommy.t.lee%40gmail.com&ctz=America/New_York" style="border: 0" width="100%" height="300" frameborder="0" scrolling="no"></iframe>
+       </div>
+
+
+
+     </div>
+
+
+
+     <div class="row">
+      <div class="four columns">
+        <div class="panel">
+          <a href='http://www.aip.org/'>
+            <img src="../images/sps/frontpage/AIP.jpg" /></a>
+          </p>
+        </div>
+      </div>
+      <div class="four columns">
+        <div class="panel">
+          <a href='http://www.spsnational.org/about/benefits.htm'>
+            <img src="../images/sps/frontpage/SPS_LOGO.jpg" /></a>
+          </p>
+        </div>
+      </div>
+      <div class="four columns">
+        <div class="panel">
+          <a href='http://www.sps.physics.ucf.edu/images/PlanetarySciencesGroup.jpg'>
+            <img src="../images/sps/frontpage/PlanetarySciencesGroup.jpg" /></a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+
+      <div class="six columns">
+        <div class="panel">
+          <a href='http://www.wolframalpha.com/'>
+            <img src="../images/sps/frontpage/WolframAlpha.jpg" /></a>
+          </p>
+        </div>
+      </div>
+      <div class="six columns">
+        <div class="panel">
+          <a href='http://mathworld.wolfram.com/'>
+            <img src="../images/sps/frontpage/WolframMathworld.jpg" /></a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class='row'>
+      <div class="twelve columns">
+
+        <ul id="next-gigs">
+          <li>No Events planned yet... <img src="http://kevin.deldycke.com/wp-includes/images/smilies/icon_sad.gif" alt=":(" class="wp-smiley"> </li>
+          <li>Feeds: <a href='http://www.google.com/calendar/feeds/coolcavemen.com_h3432m0aeeq5c6dakki50giqeo%40group.calendar.google.com/public/full?orderby=starttime&#038;sortorder=ascending&#038;futureevents=true'>RSS</a>, <a href='http://www.google.com/calendar/ical/coolcavemen.com_h3432m0aeeq5c6dakki50giqeo%40group.calendar.google.com/public/basic.ics'>iCal</a>.</li>
+        </ul>
+
+
+      </div>
+    </div>
+
+
+    <!-- End Grid Section -->
+
+
+
+    <!-- Footer -->
+<?php include 'includes/footer.php';?>
+  
+    <!-- Included JS Files (Uncompressed) -->
+  <!--
+  
+  <script src="javascripts/jquery.js"></script>
+  
+  <script src="javascripts/jquery.foundation.mediaQueryToggle.js"></script>
+  
+  <script src="javascripts/jquery.foundation.forms.js"></script>
+  
+  <script src="javascripts/jquery.foundation.reveal.js"></script>
+  
+  <script src="javascripts/jquery.foundation.orbit.js"></script>
+  
+  <script src="javascripts/jquery.foundation.navigation.js"></script>
+  
+  <script src="javascripts/jquery.foundation.buttons.js"></script>
+  
+  <script src="javascripts/jquery.foundation.tabs.js"></script>
+  
+  <script src="javascripts/jquery.foundation.tooltips.js"></script>
+  
+  <script src="javascripts/jquery.foundation.accordion.js"></script>
+  
+  <script src="javascripts/jquery.placeholder.js"></script>
+  
+  <script src="javascripts/jquery.foundation.alerts.js"></script>
+  
+  <script src="javascripts/jquery.foundation.topbar.js"></script>
+  
+  <script src="javascripts/jquery.foundation.joyride.js"></script>
+  
+  <script src="javascripts/jquery.foundation.clearing.js"></script>
+  
+  <script src="javascripts/jquery.foundation.magellan.js"></script>
+  
+-->
+
+<!-- Included JS Files (Compressed) -->
+<script src="../javascripts/jquery.js"></script>
+<script src="../javascripts/foundation.min.js"></script>
+
+<!-- Initialize JS Plugins -->
+<script src="../javascripts/app.js"></script>
+
+<script type="text/javascript">
+$(window).load(function() {
+ $('#slider').orbit();
+});
+</script>
+
+<script type="text/javascript" charset="utf-8">
+jQuery(function(){
+        // Get list of upcoming iCal events formatted in JSON
+
+        var event_url = "https://www.google.com/calendar/feeds/tommy.t.lee%40gmail.com/public/full";
+        event_url = event_url + "?orderby=starttime&sortorder=ascending&futureevents=true&alt=json";
+        jQuery.getJSON(event_url, function(data){
+            // Utility method to pad a string on the left
+            // Source: http://sajjadhossain.com/2008/10/31/javascript-string-trimming-and-padding/
+            function lpad(str, pad_string, length) {
+              var str = new String(str);
+              while (str.length < length)
+                str = pad_string + str;
+              return str;
+            };
+            // Parse and render each event
+            jQuery.each(data.feed.entry, function(i, item){
+              if(i == 0) {
+                jQuery("#next-gigs li").first().hide();
+              };
+              var event_url = jQuery.trim(item.content.$t);
+              var event_header = item.title.$t;
+              if(event_url.length > 0) {
+                event_header = "<a href='" + event_url + "'>" + event_header + "</a>";
+              };
+                // Format the date string
+                var d = new Date(item.gd$when[0].startTime);
+                var d_string = '<strong>' + d.getFullYear() + '-' + lpad((d.getMonth() + 1),'0',2) + '-' + lpad((d.getDate()),'0',2) + '</strong>';
+                if(d.getHours() != 0 || d.getMinutes() != 0) {
+                  d_string = d_string + ' at ' + lpad(d.getHours(), '0', 2) + ':' + lpad(d.getMinutes(), '0', 2);
+                };
+                // Render the event
+                jQuery("#next-gigs li").last().before(
+                  "<li><strong>"
+                  + event_header
+                  + "</strong><br/>Date: "
+                  + d_string
+                  + "<br/>Location: <a href='http://maps.google.com/maps?q="
+                  + item.gd$where[0].valueString
+                  + "' target='_blank'>"
+                  + item.gd$where[0].valueString
+                  + "</a>"
+                  + "<br />Description:"
+                  + event_url
+                  + "</li>"
+                  );
+              });
+});
+});
+</script>
+</body>
+</html>
