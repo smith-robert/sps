72b58975237739912851304d23a55d8a2dcf59b3
Tommy Lee <tommy.t.lee@gmail.com>
2012-12-27 04:25:32 -0800

    Dec26 Backup
    Backed up Last Working Copy from dev

---
+++ events.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+ <!-- Header and Nav -->
+  <?php include 'includes/header.php'; ?>
+  
+
+  <!-- End Header and Nav -->
+
+  <!-- Main Grid Section -->
+
+  <div class="row">
+
+    <div class="four columns">
+      <div class="panel">
+         <iframe src="https://www.google.com/calendar/embed?src=tommy.t.lee%40gmail.com&ctz=America/New_York" style="border: 0" width="100%" height="300" frameborder="0" scrolling="no"></iframe>
+       </div>
+       </div>
+    <div class="eight columns">
+      <div class="panel">
+      <?php include 'includes/events.php';?>
+    </div>
+  </div>
+
+     </div>
+
+
+
+    
+
+
+    <!-- End Grid Section -->
+
+
+
+    <!-- Footer -->
+<?php include 'includes/footer.php';?>
+  
+    <!-- Included JS Files (Uncompressed) -->
+  <!--
+  
+  <script src="javascripts/jquery.js"></script>
+  
+  <script src="javascripts/jquery.foundation.mediaQueryToggle.js"></script>
+  
+  <script src="javascripts/jquery.foundation.forms.js"></script>
+  
+  <script src="javascripts/jquery.foundation.reveal.js"></script>
+  
+  <script src="javascripts/jquery.foundation.orbit.js"></script>
+  
+  <script src="javascripts/jquery.foundation.navigation.js"></script>
+  
+  <script src="javascripts/jquery.foundation.buttons.js"></script>
+  
+  <script src="javascripts/jquery.foundation.tabs.js"></script>
+  
+  <script src="javascripts/jquery.foundation.tooltips.js"></script>
+  
+  <script src="javascripts/jquery.foundation.accordion.js"></script>
+  
+  <script src="javascripts/jquery.placeholder.js"></script>
+  
+  <script src="javascripts/jquery.foundation.alerts.js"></script>
+  
+  <script src="javascripts/jquery.foundation.topbar.js"></script>
+  
+  <script src="javascripts/jquery.foundation.joyride.js"></script>
+  
+  <script src="javascripts/jquery.foundation.clearing.js"></script>
+  
+  <script src="javascripts/jquery.foundation.magellan.js"></script>
+  
+-->
+
+<!-- Included JS Files (Compressed) -->
+
+<script src="../javascripts/jquery.frontpage.slider.js" type="text/javascript"></script>
+
+<script src="../javascripts/jquery.events.js" type="text/javascript" charset="utf-8"></script>
+</body>
+</html>
